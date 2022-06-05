97fe2fc206dbaf852a8043040f26ee7a3e9d48fb
Jules <j.marshallmiami@yahoo.com>
2022-06-05 06:00:23 -0500

    Prototypes added to our Player constructor

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -17,3 +17,24 @@ test('creates a player object', () => {
     );
 });
 
+// TEST SUITE 2: testing getStats() method
+test("get player's stats as an object", () => {
+    const player = new Player('Dave');
+    
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+});
+
+// TEST SUITE 3: testing getInventory() method
+test('creates a player object', () => {
+    const player = new Player('Dave');
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+    player.inventory = [];
+  
+    expect(player.getInventory()).toEqual(false);
+});
+
